e6275c1b7a9bbe6081217065c43a35c0bfdb3e25
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-09 21:24:18 +1100

    Fixed refresh page bug IE bug
    When the user refreshed the page, IE would keep the same values in the input as it did before the page refresh, this was casing an error in IE because the 24hr to 12hr conversion function was being fed 12hr time format.

--- index.js
+++ index.js
@@ -436,13 +436,16 @@ function TimePolyfill($input) {
 	}
 
 	function convert_to_12hr_time (timeString_24hr) {
-		var twentyFour_regex = /([0-9]{2})\:([0-9]{2})/;
+		// still need to support 12hr time to safeguard against page refreshes in IE
+		var twentyFour_regex = /([0-9]{2})\:([0-9]{2}) ?(AM|PM|--)/;
 		var result = twentyFour_regex.exec(timeString_24hr);
+		if (!result) return '--:-- --';
+
 		var hrs_24 = convert_number(result[1]);
 		var min = result[2];
 		var hrs_12 = convert_hours_to_12hr_time(hrs_24);
 		var isPM = hrs_24 > 12;
-		var mode = isPM ? 'PM' : 'AM';
+		var mode =  result[3] ? result[3] : isPM ? 'PM' : 'AM';
 		return [leading_zero(hrs_12), ':', min, ' ', mode].join('');
 	}
 
