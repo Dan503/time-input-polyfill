0cdbbe85abfee892f5c1cd1fe53f07eecddec22e
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-09 19:53:50 +1100

    set_time() also updates data attribute

--- index.js
+++ index.js
@@ -377,6 +377,7 @@ function TimePolyfill($input) {
 	function set_time(time_string_24hr) {
 		var twelveHr = convert_to_12hr_time(time_string_24hr);
 		$input.value = twelveHr;
+		set_data_attribute(time_string_24hr);
 	}
 
 	function set_value (segment, value) {
