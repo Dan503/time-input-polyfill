871cb5f5369f10375713ef4086f2669a0f569bcf
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-09 21:24:18 +1100

    Revert "Fixed refresh page bug IE bug"
    This reverts commit e6275c1b7a9bbe6081217065c43a35c0bfdb3e25.

--- index.js
+++ index.js
@@ -459,16 +459,13 @@ function TimePolyfill($input) {
 	}
 
 	function convert_to_12hr_time (timeString_24hr) {
-		// still need to support 12hr time to safeguard against page refreshes in IE
-		var twentyFour_regex = /([0-9]{2})\:([0-9]{2}) ?(AM|PM|--)/;
+		var twentyFour_regex = /([0-9]{2})\:([0-9]{2})/;
 		var result = twentyFour_regex.exec(timeString_24hr);
-		if (!result) return '--:-- --';
-
 		var hrs_24 = convert_number(result[1]);
 		var min = result[2];
 		var hrs_12 = convert_hours_to_12hr_time(hrs_24);
 		var isPM = hrs_24 > 12;
-		var mode =  result[3] ? result[3] : isPM ? 'PM' : 'AM';
+		var mode = isPM ? 'PM' : 'AM';
 		return [leading_zero(hrs_12), ':', min, ' ', mode].join('');
 	}
 
