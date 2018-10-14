12dcc00e161dc2b85361703c7edbefbbd4596e9a
Daniel Tonon <daniel.tonon.503@gmail.com>
2018-10-14 14:33:44 +1100

    Fixed IE11 bug

--- core/accessibility/create_a11y_block.js
+++ core/accessibility/create_a11y_block.js
@@ -4,6 +4,6 @@ module.exports = function create_accessibility_block () {
 	$block.setAttribute('aria-live', 'assertive');
 	$block.setAttribute('style', 'position: absolute; opacity: 0; height: 0; width: 0; overflow: hidden; pointer-events: none;');
 	$block.classList.add('time-input-polyfill-accessibility-block');
-	document.querySelector('body').append($block);
+	document.querySelector('body').appendChild($block);
 	return $block;
 }
