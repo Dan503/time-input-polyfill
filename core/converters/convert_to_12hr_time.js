c77d8029f16621b5f5f89da707916c634758c114
Daniel Tonon <dan503.npm@gmail.com>
2020-01-04 17:50:52 +1100

    #20 - Fix for "12:30" 24hr time being recognised as "12:30 AM"
    "12:30" in 24 hour time is equal to "12:30 PM" in 12 hour time.
    
    https://github.com/Dan503/time-input-polyfill/issues/20

--- core/converters/convert_to_12hr_time.js
+++ core/converters/convert_to_12hr_time.js
@@ -9,7 +9,7 @@ module.exports = function convert_to_12hr_time(timeString_24hr) {
   var hrs_24 = convert_number(result[1])
   var min = result[2]
   var hrs_12 = convert_hours_to_12hr_time(hrs_24)
-  var isPM = hrs_24 > 12
+  var isPM = hrs_24 >= 12
   var mode = isPM ? 'PM' : 'AM'
   return [leading_zero(hrs_12), ':', min, ' ', mode].join('')
 }
