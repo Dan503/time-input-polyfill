9455d6a652faf10d5c2ad035283705eba8c04dca
Daniel Tonon <dan503.npm@gmail.com>
2022-03-11 00:53:37 +1100

    Updating Cypress testSuite to link to tests page

--- cypress/support/testSuite.ts
+++ cypress/support/testSuite.ts
@@ -1,7 +1,5 @@
 import { TimeInputTestSuite } from '@time-input-polyfill/tests'
 
 export const testSuite = new TimeInputTestSuite({
-	localHostUrl: 'http://localhost:3000',
-	primaryTestsLabel: 'Label for the primary test input', // default = 'Primary tests'
-	eventTestsLabel: 'Label for the events test input', // default = 'Event tests'
+	localHostUrl: 'http://localhost:3000/tests',
 })
