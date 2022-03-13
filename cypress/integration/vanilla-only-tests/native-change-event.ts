921b1729d06415dd81aeb6108f4992af470c20c2
Daniel Tonon <dan503.npm@gmail.com>
2022-03-13 16:17:21 +1100

    Adding a native change event test
    The onChange event fires on blur natively where as most framworks fire onChange when the native onInput event fires.
    
    I might move this test into the main tests project later if it makes sense to do so

---
+++ cypress/integration/vanilla-only-tests/native-change-event.ts
@@ -0,0 +1,21 @@
+import { testSuite } from '../../support/testSuite'
+
+describe('native change event', () => {
+	test('native change event', () => {
+		const { loadEventsInput, use, hasEventsMainName, cySelectSegment } = testSuite.utils
+		const cyNativeChangeEventDisplay = () => cy.get('#nativeChangeEventDisplay')
+		const defaultNativeChangeText = '[native change event]'
+		loadEventsInput()
+			.then(hasEventsMainName('none'))
+			.then(cyNativeChangeEventDisplay)
+			.should('have.text', defaultNativeChangeText)
+			.then(cySelectSegment('hrs12'))
+			.then(use.upArrow)
+			.then(hasEventsMainName('change'))
+			.then(cyNativeChangeEventDisplay)
+			.should('have.text', defaultNativeChangeText)
+			.blur()
+			.then(cyNativeChangeEventDisplay)
+			.should('have.text', 'native-change')
+	})
+})
