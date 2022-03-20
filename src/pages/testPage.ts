4f3e9d7d4dae93ccbac202ef9a8ccf3b448ddbb2
Daniel Tonon <dan503.npm@gmail.com>
2022-03-20 13:21:31 +1100

    Adding click handlers for the the main test buttons

--- src/pages/testPage.ts
+++ src/pages/testPage.ts
@@ -1,7 +1,26 @@
-// import TimePolyfill from '../../core/index'
+import type { PolyfillInput } from '../../core/types'
+import { getIDsAndLabels } from '../../node_modules/@time-input-polyfill/tests/dist/mjs/src/core/IDs-and-labels'
 
-// import { getIDsAndLabels } from '../../node_modules/@time-input-polyfill/tests/dist/mjs/src/core/IDs-and-labels'
+const { IDs } = getIDsAndLabels()
+
+const { primaryInputID, buttonIDs } = IDs
 
 export function testPage() {
 
+	const inputElem = document.getElementById(primaryInputID) as PolyfillInput
+
+	document.getElementById(buttonIDs.amID)?.addEventListener('click', () => {
+		inputElem.value = '07:30'
+		inputElem.polyfill?.update()
+	})
+
+	document.getElementById(buttonIDs.pmID)?.addEventListener('click', () => {
+		inputElem.value = '15:45'
+		inputElem.polyfill?.update()
+	})
+
+	document.getElementById(buttonIDs.blankID)?.addEventListener('click', () => {
+		inputElem.value = ''
+		inputElem.polyfill?.update()
+	})
 }
