95849d9b28cc0574587ab3d296eec08f75c350df
Daniel Tonon <dan503.npm@gmail.com>
2022-03-26 19:17:13 +1100

    `e.key` means the names of the `allNumberKeys` values are different now

--- core/staticValues/allNumberKeys.ts
+++ core/staticValues/allNumberKeys.ts
@@ -1,5 +1,3 @@
-/*eslint indent: 0 */
 export const allNumberKeys: Array<string> = [
-	'Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9',
-	'Numpad0', 'Numpad1', 'Numpad2', 'Numpad3', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad7', 'Numpad8', 'Numpad9'
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 ]
