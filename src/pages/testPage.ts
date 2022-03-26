066468861872be8a2edcdf2e9fb25ef0c61e30ce
Daniel Tonon <dan503.npm@gmail.com>
2022-03-26 16:58:33 +1100

    event input tests are now passing

--- src/pages/testPage.ts
+++ src/pages/testPage.ts
@@ -1,6 +1,6 @@
 import type { PolyfillInput } from '../../core/types'
 import { getIDsAndLabels } from '../../node_modules/@time-input-polyfill/tests/dist/mjs/src/core/IDs-and-labels'
-import { EventMainName } from '../../node_modules/@time-input-polyfill/tests/dist/mjs/src/core/supportTypes'
+import { EventAltName, EventMainName } from '../../node_modules/@time-input-polyfill/tests/dist/mjs/src/core/supportTypes'
 
 const { IDs } = getIDsAndLabels()
 
@@ -9,8 +9,10 @@ const {
 	primaryCpuValueID,
 	eventsInputID,
 	eventsMainNameID,
+	eventsAltNameID,
 	formCpuValueID,
-	buttonIDs
+	buttonIDs,
+	eventsDisplayValueID,
 } = IDs
 
 export function testPage() {
@@ -19,18 +21,32 @@ export function testPage() {
 	const primaryCpuValueElem = document.getElementById(primaryCpuValueID) as HTMLParagraphElement
 
 	const eventsInputElem = document.getElementById(eventsInputID) as PolyfillInput
+	const eventsDisplayValueElem = document.getElementById(eventsDisplayValueID) as HTMLParagraphElement
 	const eventsMainNameElem = document.getElementById(eventsMainNameID) as HTMLParagraphElement
+	const eventsAltNameElem = document.getElementById(eventsAltNameID) as HTMLParagraphElement
 	const formCpuValueElem = document.getElementById(formCpuValueID) as HTMLParagraphElement
 
-	const updateCpuText = () => {
+	const updatePrimaryCpuText = () => {
 		const newValue = primaryInputElem.polyfill?.isPolyfillEnabled ? primaryInputElem.dataset.value : primaryInputElem.value
 		primaryCpuValueElem.innerText = newValue || ''
+	}
+
+	const updateEventMainName = (eventName: EventMainName) => {
+		eventsMainNameElem.innerText = eventName
+	}
+	const updateEventAltName = (eventName: EventAltName) => {
+		eventsAltNameElem.innerText = eventName
+	}
+
+	const updateEventsDisplayText = () => {
+		eventsDisplayValueElem.innerText = eventsInputElem.value
+		updateEventAltName('change')
 	}
 
 	const setTimeTo = (string24hr: string) => {
 		primaryInputElem.value = string24hr
 		primaryInputElem.polyfill?.update()
-		updateCpuText()
+		updatePrimaryCpuText()
 	}
 
 	document.getElementById(buttonIDs.amID)?.addEventListener('click', () => {
@@ -50,19 +66,41 @@ export function testPage() {
 	})
 
 	primaryInputElem.addEventListener('keyup', () => {
-		updateCpuText()
+		updatePrimaryCpuText()
 	})
 	primaryInputElem.addEventListener('keydown', () => {
-		updateCpuText()
+		updatePrimaryCpuText()
+	})
+	eventsInputElem.addEventListener('keyup', () => {
+		updateEventsDisplayText()
+		updateEventMainName('keyUp')
+	})
+	eventsInputElem.addEventListener('keydown', () => {
+		updateEventsDisplayText()
+		updateEventMainName('keyDown')
+	})
+	eventsInputElem.addEventListener('focus', () => {
+		updateEventMainName('focus')
+	})
+	eventsInputElem.addEventListener('blur', () => {
+		updateEventMainName('blur')
+	})
+	eventsInputElem.addEventListener('mousedown', () => {
+		updateEventMainName('mouseDown')
+	})
+	eventsInputElem.addEventListener('mouseup', () => {
+		updateEventMainName('mouseUp')
+	})
+	eventsInputElem.addEventListener('click', () => {
+		updateEventAltName('click')
 	})
 	eventsInputElem.form?.addEventListener('submit', () => {
 		if (eventsInputElem.form) {
 			const formData = new FormData(eventsInputElem.form)
 			// I can get away with this since there is only 1 input in the form
 			formData.forEach(d => {
-				const eventName: EventMainName = 'submit'
 				formCpuValueElem.innerHTML = d.toString()
-				eventsMainNameElem.innerText = eventName
+				updateEventMainName('submit')
 			})
 		}
 	})
