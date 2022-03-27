e1af0059fd230d9a9c300fbeaac26353e957b8a8
Daniel Tonon <dan503.npm@gmail.com>
2022-03-27 14:21:54 +1100

    Removing TODO about getting utils to support `event.code`
    Cypress doesn't support event.code so there isn't much point in updating the utils to support it

--- core/setters/manualNumberEntry.ts
+++ core/setters/manualNumberEntry.ts
@@ -5,7 +5,6 @@ export function manualNumberEntry($input: PolyfillInput, key: string) {
 	if ($input.polyfill?.isPolyfillEnabled) {
 		var key_value = key.replace(/Digit|Numpad/i, '')
 		var segment = getCursorSegment($input)
-		// TODO: manualEntryLog should be updated to work better with `event.code` values
 		$input.polyfill?.manualEntryLog[segment].add(key_value)
 	}
 }
